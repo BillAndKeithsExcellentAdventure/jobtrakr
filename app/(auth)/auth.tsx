f57bb97a0eacb9d246dea4e45f7bc520efe047c5
Bill Steinbock <bsteinbock@microtraks.com>
2025-01-22 20:14:55 -0500

    Save current state

--- app/(auth)/auth.tsx
+++ app/(auth)/auth.tsx
@@ -1,10 +1,14 @@
-import { router, Stack } from 'expo-router';
+import { Redirect, router, Stack } from 'expo-router';
 import { Text, View } from 'react-native';
 
 import { useSession } from '@/session/ctx';
 
 export default function SignIn() {
-  const { signIn } = useSession();
+  const { signIn, session } = useSession();
+  if (session) {
+    <Redirect href='/(tabs)/home' />;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Stack.Screen options={{ headerShown: true, title: 'Login to JobTrakR' }} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,77 @@
-import { ActivityIndicator, View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Redirect, Stack, useRouter, useSegments } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { SessionProvider, useSession } from '@/session/ctx';
+import { useColorScheme } from '@/components/useColorScheme';
 
-const IndexPage = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <ActivityIndicator size='large' color='#0000ff' />
-    </View>
-  );
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+const InitialLayout = () => {
+  const { session } = useSession();
+
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) return;
+
+    const inTabsGroup = segments[0] === '(auth)';
+
+    console.log('User changed: ', session);
+
+    if (session && !inTabsGroup) {
+      router.replace('/(tabs)/home');
+    } else {
+      router.replace('/(auth)/auth');
+    }
+  }, [session]);
+
+  if (!session) return <Redirect href='/(auth)/auth' />;
+
+  return <Redirect href='/(tabs)/home' />;
 };
 
-export default IndexPage;
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <SessionProvider>
+        <InitialLayout />
+      </SessionProvider>
+    </ThemeProvider>
+  );
+}
