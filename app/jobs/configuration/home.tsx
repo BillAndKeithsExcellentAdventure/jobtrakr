2bbf099b230483b4af0db91ae46c7b01f03f54a9
bsteinbock <bill.steinbock@icloud.com>
2025-03-17 14:26:47 -0400

    fix typo

--- app/jobs/configuration/home.tsx
+++ app/jobs/configuration/home.tsx
@@ -5,7 +5,7 @@ const home = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', padding: 40 }}>
       <Text txtSize="title" text="Job+" style={{ marginBottom: 5 }} />
-      <Text txtSize="normal" text="version 1.0.1" style={{ marginBottom: 20 }} />
+      <Text txtSize="standard" text="version 1.0.1" style={{ marginBottom: 20 }} />
       <Text
         txtSize="sub-title"
         text="Please use the menu icon in top left to select the item you want to configure."
