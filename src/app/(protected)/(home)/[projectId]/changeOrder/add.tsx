6161214db9f010930bddb599fd8c683b984e4308
bsteinbock <bill.steinbock@icloud.com>
2025-07-05 15:20:13 -0400

    wip

--- src/app/(protected)/(home)/[projectId]/changeOrder/add.tsx
+++ src/app/(protected)/(home)/[projectId]/changeOrder/add.tsx
@@ -10,9 +10,10 @@ import {
   useAllRows,
   useUpdateRowCallback,
 } from '@/src/tbStores/projectDetails/ProjectDetailsStoreHooks';
-import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { useColors } from '@/src/context/ColorsContext';
 import { ActionButton } from '@/src/components/ActionButton';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AddChangeOrder() {
   const { projectId } = useLocalSearchParams<{ projectId: string }>();
@@ -54,50 +55,53 @@ export default function AddChangeOrder() {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.header}>Add Change Order</Text>
-      <Text style={styles.label}>Title</Text>
-      <TextInput
-        style={styles.input}
-        value={newChangeOrder.title}
-        onChangeText={(text) => handleChange('title', text)}
-        placeholder="Title"
-      />
-      <Text style={styles.label}>Description</Text>
-      <TextInput
-        style={[styles.input, { height: 80 }]}
-        value={newChangeOrder.description}
-        onChangeText={(text) => handleChange('description', text)}
-        placeholder="Description"
-        numberOfLines={4}
-        multiline
-      />
-      <Text style={styles.label}>Amount</Text>
-      <TextInput
-        style={styles.input}
-        value={newChangeOrder.bidAmount ? String(newChangeOrder.bidAmount) : ''}
-        onChangeText={(text) => handleChange('bidAmount', text)}
-        placeholder="Amount"
-        keyboardType="numeric"
-      />
-      <View style={styles.saveButtonRow}>
-        <ActionButton
-          style={styles.saveButton}
-          onPress={handleSubmit}
-          type={canAdd ? 'ok' : 'disabled'}
-          title="Save"
-        />
+    <SafeAreaView edges={['right', 'bottom', 'left']} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: 'Add Receipt', headerShown: true }} />
 
-        <ActionButton
-          style={styles.cancelButton}
-          onPress={() => {
-            router.back();
-          }}
-          type={'cancel'}
-          title="Cancel"
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          value={newChangeOrder.title}
+          onChangeText={(text) => handleChange('title', text)}
+          placeholder="Title"
         />
-      </View>
-    </ScrollView>
+        <Text style={styles.label}>Description</Text>
+        <TextInput
+          style={[styles.input, { height: 80 }]}
+          value={newChangeOrder.description}
+          onChangeText={(text) => handleChange('description', text)}
+          placeholder="Description"
+          numberOfLines={4}
+          multiline
+        />
+        <Text style={styles.label}>Amount</Text>
+        <TextInput
+          style={styles.input}
+          value={newChangeOrder.bidAmount ? String(newChangeOrder.bidAmount) : ''}
+          onChangeText={(text) => handleChange('bidAmount', text)}
+          placeholder="Amount"
+          keyboardType="numeric"
+        />
+        <View style={styles.saveButtonRow}>
+          <ActionButton
+            style={styles.saveButton}
+            onPress={handleSubmit}
+            type={canAdd ? 'ok' : 'disabled'}
+            title="Save"
+          />
+
+          <ActionButton
+            style={styles.cancelButton}
+            onPress={() => {
+              router.back();
+            }}
+            type={'cancel'}
+            title="Cancel"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
