e0bbd66559e15de97d3187d5b6dbd6c434d0b8f7
bsteinbock <bill.steinbock@icloud.com>
2025-09-23 11:06:32 -0400

    minor app config change

--- app.config.ts
+++ app.config.ts
@@ -158,11 +158,11 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       projectId: '0d1178cf-26f4-4ce6-8014-1a3b95e0f7e5',
     },
   },
-  owner: 'bkea',
   runtimeVersion: {
     policy: 'appVersion',
   },
   updates: {
     url: 'https://u.expo.dev/0d1178cf-26f4-4ce6-8014-1a3b95e0f7e5',
   },
+  owner: 'bkea',
 });
