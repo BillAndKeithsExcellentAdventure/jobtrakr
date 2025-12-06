4a873d9f7b6047b4466b977a2684a9a28a82453b
bsteinbock <bill.steinbock@icloud.com>
2025-12-05 22:46:30 -0500

    set back button color

--- src/components/StyledHeaderBackButton.tsx
+++ src/components/StyledHeaderBackButton.tsx
@@ -1,14 +1,22 @@
 import { HeaderBackButton, HeaderBackButtonProps } from '@react-navigation/elements';
 import React from 'react';
 import { Platform, StyleSheet } from 'react-native';
-
+import { useColors } from '@/src/context/ColorsContext';
 /**
  * A styled HeaderBackButton that matches the default expo-router header styling.
  * This addresses the issue where the default HeaderBackButton from @react-navigation/elements
  * has too much left margin when compared to the default expo-router header.
  */
 export const StyledHeaderBackButton: React.FC<HeaderBackButtonProps> = (props) => {
-  return <HeaderBackButton {...props} style={[styles.backButton, props.style]} />;
+  const colors = useColors();
+
+  return (
+    <HeaderBackButton
+      {...props}
+      style={[styles.backButton, props.style]}
+      tintColor={Platform.OS === 'ios' ? '#007AFF' : colors.text}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
